6c05e6039bf51cef6aa8018e81e8b664ce2e7761
Jsebasj96 <jjimenezt2@ucentral.edu.co>
2025-04-10 12:59:25 -0500

    Creacion Reservas Hoteles 2

--- backend/models/HotelBooking.js
+++ backend/models/HotelBooking.js
@@ -1,14 +1,11 @@
 const pool = require("../config/db");
 
-// Crear reserva de hotel
+// Crear reserva de hotels
 const createHotelBooking = async (userId, hotelId, checkIn, checkOut) => {
-  const totalPrice = 200000; // ✅ Puedes calcularlo dinámicamente si quieres
-  const status = "PENDING";
-
   const result = await pool.query(
-    `INSERT INTO hotel_bookings (user_id, hotel_id, check_in, check_out, total_price, booking_date, status) 
-     VALUES ($1, $2, $3, $4, $5, NOW(), $6) RETURNING *`,
-    [userId, hotelId, checkIn, checkOut, totalPrice, status]
+    `INSERT INTO hotel_bookings (user_id, hotel_id, check_in, check_out) 
+     VALUES ($1, $2, $3, $4) RETURNING *`,
+    [userId, hotelId, checkIn, checkOut]
   );
   return result.rows[0];
 };
