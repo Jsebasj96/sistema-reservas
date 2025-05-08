1f57a6c79daa516216c45fdceab164e10cc7fb56
Jsebasj96 <jjimenezt2@ucentral.edu.co>
2025-05-08 14:25:38 -0500

    Eliminar importación de constants.js y usar process.env.SECRET_KEY

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../config/constants');
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
